cd1185f0b2d3c867565510ab57d3a216dfce55cd
Augustine <augustineeneh7@gmail.com>
2023-07-14 21:58:57 -0700

    Variables, if, else, while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'print numbers of base 10'
+ * description: 'print number of base 10 using putchar'
  * Return: Always 0
  */
 int main(void)
 {
 int n;
 
-for (n = 0; n <= 9, n++;)
+for (n = 0; n <= 9; n++)
 	{
-printf("%d", n);
+putchar(n + 48);
 	}
-printf("\n");
+putchar('\n');
 return (0);
 }
